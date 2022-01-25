62941b11ac21bc61761deb31952d8d85c395b749
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-25 00:04:09 -0500

    Refactor: added ; to line 7

--- Develop/routes/htmlRoute.js
+++ Develop/routes/htmlRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 //express router get the notes from app
 router.get("/notes", function (req, res) {
     //joins notes that are sent to notes.html 
-    res.send(path.join(__dirname,'.../public/notes.html'))
+    res.send(path.join(__dirname,'.../public/notes.html'));
 });
 
 // allows for the router module to be exported to other files

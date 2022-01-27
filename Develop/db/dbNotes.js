e918e33af6af7a842c16556042f9ae8fe214eb2d
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-27 15:05:10 -0500

    REFACTOR: error message was due to .get not matching' INSTALLED: error message resolved after installing npm install uuid

--- Develop/db/dbNotes.js
+++ Develop/db/dbNotes.js
@@ -3,3 +3,13 @@ const fs = require("fs");
 
 
 const uuid = require("uuid/v1");
+
+//create class (Store) w/ same file name 
+    //needs 5 methods 
+    // read fs. read file
+    // wrtie fs.writeFile
+    // get fs.getNotes > @ db.json & parse
+    // add note fs.adNotes
+    // remove fs.removeNotes 
+
+    module.exports = Store
--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -1,26 +1,27 @@
-const express = require("express").Router;
-const htmlRoute = require("./htmlRoute");
-const app = express();
+const router = require("express").Router;
+const store = require("../db/dbNotes")
+
  
-app.get("/Develop/notes", function (req, res) {
-    //requesting htmlRoute to get notes.html and put into db template
-    htmlRoute.getNotes(dbStructure); 
+router.get("/notes", function (req, res) {
+   store.getnotes().then((notes) => {
+       return res.json(notes);
+   })
 });
 
-app.post("/Develop/notes", function (req, res) {
+router.post("/notes", function (req, res) {
     //body if pushed into db template if json response is received
-    dbStructure.push(req.body);
-    res.json(true);
+    // dbStructure.push(req.body);
+    // res.json(true);
 });
     
 
-app.delete("/Develop/notes", function (req, res) {
+router.delete("/notes", function (req, res) {
     // boolean if/else statement that will show a 500 status if response is empty
-    if (dbStructure.length != 0) {
-        res.json(true);
-    } else {
-        res.status(500).json(err);
-    }
+    // if (dbStructure.length != 0) {
+    //     res.json(true);
+    // } else {
+    //     res.status(500).json(err);
+    // }
 });
 
 

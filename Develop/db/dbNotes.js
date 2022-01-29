8b2bee798b90a748295809a8c9fc158d34de01f4
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-29 17:02:55 -0500

    REFACTOR: deleted redundant code

--- Develop/db/dbNotes.js
+++ Develop/db/dbNotes.js
@@ -7,9 +7,11 @@ const uuid = require("uuid/v1");
 //create class (Store) w/ same file name 
     //needs 5 methods 
     // read fs. read file
-    // wrtie fs.writeFile
+    // write fs.writeFile
     // get fs.getNotes > @ db.json & parse
     // add note fs.adNotes
     // remove fs.removeNotes 
 
+
+    // get route > post > delete in apiRoutes.js 
     module.exports = Store

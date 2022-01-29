30967f1d075ba7000c07041458bde56e140285e3
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-29 18:09:42 -0500

    CREATED: functions "

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -5,12 +5,21 @@ const fs = require("fs");
 const uuid = require("uuid/v1");
 
 //create class (Store) w/ same file name 
-    //needs 5 methods 
-    // read fs. read file
+class Store {
+    
+    // fs will read file
+    read () {
+    fs.readFile("./db/db.json", "utf8");
+    
+    
+    
+    
     // write fs.writeFile
     // get fs.getNotes > @ db.json & parse
     // add note fs.adNotes
     // remove fs.removeNotes 
+
+};
 
 
     // get route > post > delete in apiRoutes.js 

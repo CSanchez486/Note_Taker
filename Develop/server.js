3d60a2b817169cbbb013e065f0863febb1617a3c
Christian Sanchez <csanchezmartinez486@gmail.com>
2022-01-29 20:17:14 -0500

    added DS_Store to .gitignore

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const apiRoute = require("./routes/apiRoute");
 const htmlRoute = require("./routes/htmlRoute");
-
-// requires db.json to be used 
-const db = require("./db/db.json");
+const app = express();
 const PORT = process.env.PORT || 3001;
 
-const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-
 // pulls htmlRoute
 app.use("/", htmlRoute);
-
 // pulls apiRoute
 app.use("/api/", apiRoute);
 
